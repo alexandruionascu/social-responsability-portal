5a8b01490efb3702edccb6301dd594890e78759a
Alexandru Ionascu <alexandruionascu.bd@gmail.com>
2017-12-08 15:23:50 +0200

    added react-router-dom

---
+++ src/LoginPage.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+class LoginPage extends React.Component<any, any> {
+    render() {
+        return (<h1>Login Page</h1>);
+    }
+}
+
+export default LoginPage;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import LoginPage from './LoginPage';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={App}/>
+      <Route exact path="/login" component={LoginPage}/>
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById('root') as HTMLElement);
 registerServiceWorker();
