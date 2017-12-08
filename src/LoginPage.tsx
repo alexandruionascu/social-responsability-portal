60fe9ba9159860f16e8c73d52f23d5fd2cbb2858
Alexandru Ionascu <alexandruionascu.bd@gmail.com>
2017-12-08 16:08:45 +0200

    started working at login page

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -1,8 +1,86 @@
 import * as React from 'react';
+import {Button, Col, Row, Icon, Input} from 'react-materialize';
 
-class LoginPage extends React.Component<any, any> {
+const materialCityBackground = require('./images/material_city.jpg');
+
+const leftColStyle = {
+    color: 'black'
+};
+
+const noMarginRow = {
+    marginBottom: 0,
+    padding: 20
+};
+
+const buttonStyle = {
+    textTransform: 'none'
+};
+
+class LoginPage extends React.Component < any,
+any > {
     render() {
-        return (<h1>Login Page</h1>);
+        return (
+            <div
+                className="valign-wrapper"
+                style={{
+                height: '100vh',
+                background: "white",
+                fontWeight: 700
+            }}>
+                <div
+                    style={{
+                    background: 'white',
+                    margin: '0 auto',
+                    color: 'white',
+                    fontWeight: 500
+                }}>
+                    <Row
+                        style={{
+                        border: '1px solid black',
+                        marginBottom: 0,
+                        background: `url(${materialCityBackground})`,
+                        backgroundSize: '50% 100%',
+                        boxShadow: '0 0 5px 2px black',
+                        backgroundRepeat: 'no-repeat'
+                    }}>
+                        <Row style={noMarginRow}>
+                            <Col s={6}>
+                                <h1>Social Portal</h1>
+                            </Col>
+
+                            <Col s={6} className="center-align" style={leftColStyle}>
+                                <h1>Login</h1>
+                                <Input s={10} label="email" validate type='email'>
+                                    <Icon>email</Icon>
+                                </Input>
+                            </Col>
+                        </Row>
+
+                        <Row style={noMarginRow}>
+                            <Col s={6}>
+                                <h4>Track local issues right now!</h4>
+                            </Col>
+                            <Col s={6} className="center-align" style={leftColStyle}>
+                                <Input s={10} type='password' label="password" validate>
+                                    <Icon>lock</Icon>
+                                </Input>
+                            </Col>
+                        </Row>
+
+                        <Row style={noMarginRow}>
+                            <Col s={6}>
+                                <Button style={buttonStyle}>Sign Up</Button>
+                            </Col>
+                            <Col s={6} className="center-align" style={leftColStyle}>
+                                <Button style={buttonStyle}>Login In</Button>
+                            </Col>
+                        </Row>
+
+                    </Row>
+                </div>
+
+            </div>
+        );
     }
 }
 
