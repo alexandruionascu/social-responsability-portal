4344190aa65898b30ddf5f4beffbad9328f6c652
Alexandru Ionascu <alexandruionascu.bd@gmail.com>
2017-12-09 15:04:15 +0200

    added json parser

--- src/server/routes/User.ts
+++ src/server/routes/User.ts
@@ -1,8 +1,14 @@
 import {Router, Request, Response} from 'express';
 const users: Router = Router();
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 
 users.get('/', function(req: Request, res: Response) {
     res.send('hello world');
+});
+
+users.post('/', jsonParser, function(req: Request, res: Response) {
+    console.log(req.body);
 });
 
 export default users;
